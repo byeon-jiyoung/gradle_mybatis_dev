3d455cb02bac345c48936454e683bb1397617d1b
변지영 <bjy8668@hanmail.net>
2019-07-18 17:02:39 +0900

    310p까지

---
+++ src/main/java/kr/or/yi/gradle_mybatis_dev/dao/TutorMapperImpl.java
@@ -0,0 +1,18 @@
+package kr.or.yi.gradle_mybatis_dev.dao;
+
+import org.apache.ibatis.session.SqlSession;
+
+import kr.or.yi.gradle_mybatis_dev.dto.Tutor;
+import kr.or.yi.gradle_mybatis_dev.jdbc.MybatisSqlSessionFactory;
+
+public class TutorMapperImpl implements TutorMapper {
+	private String namespace = "kr.or.yi.gradle_mybatis_dev.dao.TutorMapper.";
+	
+	@Override
+	public Tutor selectTutorByTutorId(Tutor tutor) {
+		try(SqlSession sqlSession = MybatisSqlSessionFactory.openSession()) {
+			return sqlSession.selectOne(namespace + "selectTutorByTutorId", tutor);
+		}
+	}
+
+}
